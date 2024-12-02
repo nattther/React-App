38ca40dceee53d2161b82bb085a44d6bc3688764
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 12:20:54 +0100

    :hammer: user can put values.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 import Title from './Title';
 import Counter from './Counter';
 import CounterContainer from './CounterContainer';
+import { useState } from 'react';
 
 function App() {
+
+  const [limiteValue, setLimiteValue] = useState(5);
+  const [minValue, setMinValue] = useState(0);
+  const [maxvalue, setMaxValue] = useState(10);
 
   const appInfo = {
     Name: 'mon app',
     Message: 'mon compteur'
   }
   const counterConstants = {
-    minValue: 0,
-    limiteValue: 5,
-    maxValue: 10
+    minValue: minValue,
+    limiteValue: limiteValue,
+    maxValue: maxvalue  }
+
+  const onChangeLimiteValueInput = (e) =>{
+    setLimiteValue(e.target.value);
+  }
+  const onChangeMinValueInput = (e) =>{
+    setMinValue(e.target.value);
+  }
+  const onChangeMaxValueInput = (e) =>{
+    setMaxValue(e.target.value);
   }
 
   return (
     <div className="App">
       <Title myMessage={appInfo.Message}></Title>
+      <p>Limite value</p>
+      <input onChange={onChangeLimiteValueInput}></input>
+      <p>Min value</p>
+      <input onChange={onChangeMinValueInput}></input>
+      <p>Max value</p>
+      <input onChange={onChangeMaxValueInput}></input>
       <CounterContainer>
       <Counter {...counterConstants}/>
         </CounterContainer>
