4b5e1330af8411f2f184833eb8a907d35fe9130d
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 11:59:13 +0100

    other props methode.

--- src/Counter.js
+++ src/Counter.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
-function Counter({...props}) {
-    const limiteValue = props.limiteValue
-    const maxValue = props.maxValue
-    const minValue = props.minValue
+function Counter({ ...props }) {
+  //const limiteValue = props.limiteValue;
+  //const maxValue = props.maxValue;
+  // const minValue = props.minValue;
+  const { limiteValue, maxValue, minValue } = { ...props };
   const [counter, setCounter] = useState(0);
   const [isOver, setIsOver] = useState(false);
 
@@ -30,13 +31,22 @@ function Counter({...props}) {
   return (
     <>
       <p>{counter}</p>
-      <button disabled={counter >=maxValue}  onClick={increment}> Increment Counter </button>
-      <button disabled={counter <= minValue} onClick={decrement}> Decrement Counter </button>
+      <button disabled={counter >= maxValue} onClick={increment}>
+        {" "}
+        Increment Counter{" "}
+      </button>
+      <button disabled={counter <= minValue} onClick={decrement}>
+        {" "}
+        Decrement Counter{" "}
+      </button>
       {counter !== limiteValue && (
         <>
           {" "}
           {isOver ? (
-            <p>Mon compteur est au dessus de {limiteValue} et sa valeur est {counter} </p>
+            <p>
+              Mon compteur est au dessus de {limiteValue} et sa valeur est{" "}
+              {counter}{" "}
+            </p>
           ) : (
             <p> Mon compteur est en dessous de {limiteValue} </p>
           )}{" "}
