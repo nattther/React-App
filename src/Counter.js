ce44042cb154a2f1211a2e498fff4d05bd91ebee
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 10:42:20 +0100

    🔨  Add conditions.

--- src/Counter.js
+++ src/Counter.js
@@ -3,28 +3,38 @@ import { useState } from "react";
 function Counter() {
   const [counter, setCounter] = useState(0);
   const [isOver5, setIsOver5] = useState(false);
+
   const increment = () => {
-    setCounter(counter + 1);
+    const newValue = counter + 1;
+    setCounter(newValue);
+    isOver5Setter(newValue);
 
-    const decrement = () => {
-      setCounter(counter - 1);
-    };
-
-    const isOver5Setter = () => {
-      if (counter > 5) {
-        setIsOver5(true);
-      } else {
-        setIsOver5(false);
-      }
-    };
   };
+
+  const decrement = () => {
+    const newValue = counter - 1;
+    setCounter(newValue);
+    isOver5Setter(newValue);
+
+  };
+
+  const isOver5Setter = (newValue) => {
+    if (newValue >= 5) {
+      setIsOver5(true);
+    } else {
+      setIsOver5(false);
+    }
+  };
+
 
   return (
     <>
       <p>{counter}</p>
       <button onClick={increment}> Increment Counter </button>
       <button onClick={decrement}> Decrement Counter </button>
-      {isOver5 && <p>Mon compteur est au dessus de 5</p>}
+      { counter !== 5 &&
+       <> {isOver5 ?  <p>Mon compteur est au dessus de 5 </p> : <p> Mon compteur est en dessous de 5 </p>} </>
+       }
     </>
   );
 }
