8d4ed4b05f612d177b618998ad3d12cc0f02533a
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 10:28:23 +0100

    :hammer: create mess.

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
-function Counter(){
-    const [counter,setCounter] = useState(0);
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [isOver5, setIsOver5] = useState(false);
+  const increment = () => {
+    setCounter(counter + 1);
 
+    const decrement = () => {
+      setCounter(counter - 1);
+    };
 
-    return(<>
-    <p>
-        {counter}
-    </p>
-    <button> Increment Counter </button>
-    <button> Decrement Counter </button>
-    </>);
+    const isOver5Setter = () => {
+      if (counter > 5) {
+        setIsOver5(true);
+      } else {
+        setIsOver5(false);
+      }
+    };
+  };
+
+  return (
+    <>
+      <p>{counter}</p>
+      <button onClick={increment}> Increment Counter </button>
+      <button onClick={decrement}> Decrement Counter </button>
+      {isOver5 && <p>Mon compteur est au dessus de 5</p>}
+    </>
+  );
 }
 
-export default Counter
+export default Counter;
