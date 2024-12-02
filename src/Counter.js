4c77afc042836acffe34dabf37610be9e0cfecdd
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2024-12-02 11:44:29 +0100

    :sparkles: creation objet

--- src/Counter.js
+++ src/Counter.js
@@ -1,41 +1,41 @@
 import { useState } from "react";
 
-function Counter() {
+function Counter({limiteValue , maxValue , minValue}) {
   const [counter, setCounter] = useState(0);
-  const [isOver5, setIsOver5] = useState(false);
+  const [isOver, setIsOver] = useState(false);
 
   const increment = () => {
     const newValue = counter + 1;
     setCounter(newValue);
-    isOver5Setter(newValue);
+    isOverSetter(newValue);
   };
 
   const decrement = () => {
     const newValue = counter - 1;
     setCounter(newValue);
-    isOver5Setter(newValue);
+    isOverSetter(newValue);
   };
 
-  const isOver5Setter = (newValue) => {
-    if (newValue >= 5) {
-      setIsOver5(true);
+  const isOverSetter = (actualCounter) => {
+    if (actualCounter >= limiteValue) {
+      setIsOver(true);
     } else {
-      setIsOver5(false);
+      setIsOver(false);
     }
   };
 
   return (
     <>
       <p>{counter}</p>
-      <button onClick={increment}> Increment Counter </button>
+      <button disabled={counter >=maxValue}  onClick={increment}> Increment Counter </button>
       <button onClick={decrement}> Decrement Counter </button>
-      {counter !== 5 && (
+      {counter !== limiteValue && (
         <>
           {" "}
-          {isOver5 ? (
-            <p>Mon compteur est au dessus de 5 et sa valeur est {counter} </p>
+          {isOver ? (
+            <p>Mon compteur est au dessus de {limiteValue} et sa valeur est {counter} </p>
           ) : (
-            <p> Mon compteur est en dessous de 5 </p>
+            <p> Mon compteur est en dessous de {limiteValue} </p>
           )}{" "}
         </>
       )}
